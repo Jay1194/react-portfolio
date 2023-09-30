1ac520389d1cfc4648f02c047ed6bc8bc059cbad
Jay <j.thomastt1@gmail.com>
2023-09-30 12:48:08 -0600

    made editions

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,16 +11,17 @@ const Main = () => {
             <div className="typeEffect">
                 <Typewriter
                 onInit={(typewriter) => {
-            typewriter.typeString("Welcome to Jayden Taylors Professional Portfolio").pauseFor(4000)
+            typewriter.typeString("Welcome to Jayden Taylor's Professional Portfolio").pauseFor(5000)
             .deleteAll()
             typewriter.typeString('Powered By React').pauseFor(2000)
             .deleteAll()
+            typewriter.typeString("Don't hesitate to explore my outstanding portfolio!")
             .start()
         }}
         />
             </div>
         </div>
-    </div>
+    </div> 
     )
 }
 
